4bced5fea343279ddcf98e94487d9d3447073905
Marton Zoltan <martonzoli99@gmail.com>
2020-02-14 10:07:01 +0100

    Create

---
+++ Forma1/Form1Form.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Forma1
+{
+    partial class Form1Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonInitializeData = new System.Windows.Forms.Button();
+            this.buttonDeleteTeam = new System.Windows.Forms.Button();
+            this.buttonUpdateTeam = new System.Windows.Forms.Button();
+            this.buttonAddTeam = new System.Windows.Forms.Button();
+            this.labelTeamName = new System.Windows.Forms.Label();
+            this.textBoxTeamName = new System.Windows.Forms.TextBox();
+            this.labelTeam = new System.Windows.Forms.Label();
+            this.listBoxTeam = new System.Windows.Forms.ListBox();
+            this.buttonDeleteRacer = new System.Windows.Forms.Button();
+            this.buttonUpdateRacer = new System.Windows.Forms.Button();
+            this.buttonAddRacer = new System.Windows.Forms.Button();
+            this.labelRecer = new System.Windows.Forms.Label();
+            this.listBoxRacer = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxRacerSalary = new System.Windows.Forms.TextBox();
+            this.textBoxRacerAge = new System.Windows.Forms.TextBox();
+            this.textBoxRacerName = new System.Windows.Forms.TextBox();
+            this.labelSalary = new System.Windows.Forms.Label();
+            this.labelRacerAge = new System.Windows.Forms.Label();
+            this.labelRacerName = new System.Windows.Forms.Label();
+            this.comboBoxTeamMove = new System.Windows.Forms.ComboBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonComputeTeamSalary = new System.Windows.Forms.Button();
+            this.textBoxTeamSalary = new System.Windows.Forms.TextBox();
+            this.buttonComputeF1Salary = new System.Windows.Forms.Button();
+            this.textBoxF1Salary = new System.Windows.Forms.TextBox();
+            this.buttonMoveToTeam = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonInitializeData
+            // 
+            this.buttonInitializeData.Location = new System.Drawing.Point(12, 22);
+            this.buttonInitializeData.Name = "buttonInitializeData";
+            this.buttonInitializeData.Size = new System.Drawing.Size(180, 23);
+            this.buttonInitializeData.TabIndex = 15;
+            this.buttonInitializeData.Text = "Teszt adatok betöltése";
+            this.buttonInitializeData.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteTeam
+            // 
+            this.buttonDeleteTeam.Location = new System.Drawing.Point(37, 564);
+            this.buttonDeleteTeam.Name = "buttonDeleteTeam";
+            this.buttonDeleteTeam.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeleteTeam.TabIndex = 14;
+            this.buttonDeleteTeam.Text = "Töröl";
+            this.buttonDeleteTeam.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdateTeam
+            // 
+            this.buttonUpdateTeam.Location = new System.Drawing.Point(37, 535);
+            this.buttonUpdateTeam.Name = "buttonUpdateTeam";
+            this.buttonUpdateTeam.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdateTeam.TabIndex = 13;
+            this.buttonUpdateTeam.Text = "Módosít";
+            this.buttonUpdateTeam.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddTeam
+            // 
+            this.buttonAddTeam.Location = new System.Drawing.Point(37, 506);
+            this.buttonAddTeam.Name = "buttonAddTeam";
+            this.buttonAddTeam.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddTeam.TabIndex = 12;
+            this.buttonAddTeam.Text = "Hozzáad";
+            this.buttonAddTeam.UseVisualStyleBackColor = true;
+            // 
+            // labelTeamName
+            // 
+            this.labelTeamName.AutoSize = true;
+            this.labelTeamName.Location = new System.Drawing.Point(34, 442);
+            this.labelTeamName.Name = "labelTeamName";
+            this.labelTeamName.Size = new System.Drawing.Size(70, 13);
+            this.labelTeamName.TabIndex = 11;
+            this.labelTeamName.Text = "Csapat neve:";
+            // 
+            // textBoxTeamName
+            // 
+            this.textBoxTeamName.Location = new System.Drawing.Point(37, 458);
+            this.textBoxTeamName.Name = "textBoxTeamName";
+            this.textBoxTeamName.Size = new System.Drawing.Size(155, 20);
+            this.textBoxTeamName.TabIndex = 10;
+            // 
+            // labelTeam
+            // 
+            this.labelTeam.AutoSize = true;
+            this.labelTeam.Location = new System.Drawing.Point(34, 59);
+            this.labelTeam.Name = "labelTeam";
+            this.labelTeam.Size = new System.Drawing.Size(98, 13);
+            this.labelTeam.TabIndex = 9;
+            this.labelTeam.Text = "Forma 1. csapatok:";
+            // 
+            // listBoxTeam
+            // 
+            this.listBoxTeam.FormattingEnabled = true;
+            this.listBoxTeam.Location = new System.Drawing.Point(37, 87);
+            this.listBoxTeam.Name = "listBoxTeam";
+            this.listBoxTeam.Size = new System.Drawing.Size(155, 329);
+            this.listBoxTeam.TabIndex = 8;
+            // 
+            // buttonDeleteRacer
+            // 
+            this.buttonDeleteRacer.Location = new System.Drawing.Point(224, 178);
+            this.buttonDeleteRacer.Name = "buttonDeleteRacer";
+            this.buttonDeleteRacer.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeleteRacer.TabIndex = 22;
+            this.buttonDeleteRacer.Text = "Töröl";
+            this.buttonDeleteRacer.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdateRacer
+            // 
+            this.buttonUpdateRacer.Location = new System.Drawing.Point(122, 178);
+            this.buttonUpdateRacer.Name = "buttonUpdateRacer";
+            this.buttonUpdateRacer.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdateRacer.TabIndex = 21;
+            this.buttonUpdateRacer.Text = "Módosít";
+            this.buttonUpdateRacer.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddRacer
+            // 
+            this.buttonAddRacer.Location = new System.Drawing.Point(18, 178);
+            this.buttonAddRacer.Name = "buttonAddRacer";
+            this.buttonAddRacer.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddRacer.TabIndex = 20;
+            this.buttonAddRacer.Text = "Hozzáad";
+            this.buttonAddRacer.UseVisualStyleBackColor = true;
+            // 
+            // labelRecer
+            // 
+            this.labelRecer.AutoSize = true;
+            this.labelRecer.Location = new System.Drawing.Point(264, 59);
+            this.labelRecer.Name = "labelRecer";
+            this.labelRecer.Size = new System.Drawing.Size(114, 13);
+            this.labelRecer.TabIndex = 17;
+            this.labelRecer.Text = "Forma 1. csapat tagjai:";
+            // 
+            // listBoxRacer
+            // 
+            this.listBoxRacer.FormattingEnabled = true;
+            this.listBoxRacer.Location = new System.Drawing.Point(267, 87);
+            this.listBoxRacer.Name = "listBoxRacer";
+            this.listBoxRacer.Size = new System.Drawing.Size(297, 329);
+            this.listBoxRacer.TabIndex = 16;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxRacerSalary);
+            this.panel1.Controls.Add(this.textBoxRacerAge);
+            this.panel1.Controls.Add(this.textBoxRacerName);
+            this.panel1.Controls.Add(this.labelSalary);
+            this.panel1.Controls.Add(this.labelRacerAge);
+            this.panel1.Controls.Add(this.labelRacerName);
+            this.panel1.Controls.Add(this.buttonDeleteRacer);
+            this.panel1.Controls.Add(this.buttonAddRacer);
+            this.panel1.Controls.Add(this.buttonUpdateRacer);
+            this.panel1.Location = new System.Drawing.Point(609, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 240);
+            this.panel1.TabIndex = 23;
+            // 
+            // textBoxRacerSalary
+            // 
+            this.textBoxRacerSalary.Location = new System.Drawing.Point(86, 96);
+            this.textBoxRacerSalary.Name = "textBoxRacerSalary";
+            this.textBoxRacerSalary.Size = new System.Drawing.Size(235, 20);
+            this.textBoxRacerSalary.TabIndex = 28;
+            // 
+            // textBoxRacerAge
+            // 
+            this.textBoxRacerAge.Location = new System.Drawing.Point(86, 52);
+            this.textBoxRacerAge.Name = "textBoxRacerAge";
+            this.textBoxRacerAge.Size = new System.Drawing.Size(235, 20);
+            this.textBoxRacerAge.TabIndex = 27;
+            // 
+            // textBoxRacerName
+            // 
+            this.textBoxRacerName.Location = new System.Drawing.Point(51, 16);
+            this.textBoxRacerName.Name = "textBoxRacerName";
+            this.textBoxRacerName.Size = new System.Drawing.Size(270, 20);
+            this.textBoxRacerName.TabIndex = 26;
+            // 
+            // labelSalary
+            // 
+            this.labelSalary.AutoSize = true;
+            this.labelSalary.Location = new System.Drawing.Point(15, 99);
+            this.labelSalary.Name = "labelSalary";
+            this.labelSalary.Size = new System.Drawing.Size(43, 13);
+            this.labelSalary.TabIndex = 26;
+            this.labelSalary.Text = "Fizetés:";
+            // 
+            // labelRacerAge
+            // 
+            this.labelRacerAge.AutoSize = true;
+            this.labelRacerAge.Location = new System.Drawing.Point(15, 59);
+            this.labelRacerAge.Name = "labelRacerAge";
+            this.labelRacerAge.Size = new System.Drawing.Size(43, 13);
+            this.labelRacerAge.TabIndex = 25;
+            this.labelRacerAge.Text = "Életkor:";
+            // 
+            // labelRacerName
+            // 
+            this.labelRacerName.AutoSize = true;
+            this.labelRacerName.Location = new System.Drawing.Point(15, 23);
+            this.labelRacerName.Name = "labelRacerName";
+            this.labelRacerName.Size = new System.Drawing.Size(30, 13);
+            this.labelRacerName.TabIndex = 24;
+            this.labelRacerName.Text = "Név:";
+            // 
+            // comboBoxTeamMove
+            // 
+            this.comboBoxTeamMove.FormattingEnabled = true;
+            this.comboBoxTeamMove.Location = new System.Drawing.Point(267, 457);
+            this.comboBoxTeamMove.Name = "comboBoxTeamMove";
+            this.comboBoxTeamMove.Size = new System.Drawing.Size(186, 21);
+            this.comboBoxTeamMove.TabIndex = 24;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(883, 564);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 29;
+            this.buttonExit.Text = "Kilépés";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // buttonComputeTeamSalary
+            // 
+            this.buttonComputeTeamSalary.Location = new System.Drawing.Point(267, 526);
+            this.buttonComputeTeamSalary.Name = "buttonComputeTeamSalary";
+            this.buttonComputeTeamSalary.Size = new System.Drawing.Size(297, 23);
+            this.buttonComputeTeamSalary.TabIndex = 30;
+            this.buttonComputeTeamSalary.Text = "Csapat össz bérkifeizetése:";
+            this.buttonComputeTeamSalary.UseVisualStyleBackColor = true;
+            // 
+            // textBoxTeamSalary
+            // 
+            this.textBoxTeamSalary.Location = new System.Drawing.Point(267, 567);
+            this.textBoxTeamSalary.Name = "textBoxTeamSalary";
+            this.textBoxTeamSalary.ReadOnly = true;
+            this.textBoxTeamSalary.Size = new System.Drawing.Size(297, 20);
+            this.textBoxTeamSalary.TabIndex = 31;
+            // 
+            // buttonComputeF1Salary
+            // 
+            this.buttonComputeF1Salary.Location = new System.Drawing.Point(37, 618);
+            this.buttonComputeF1Salary.Name = "buttonComputeF1Salary";
+            this.buttonComputeF1Salary.Size = new System.Drawing.Size(155, 23);
+            this.buttonComputeF1Salary.TabIndex = 32;
+            this.buttonComputeF1Salary.Text = "F1 össz bérkifeizetése:";
+            this.buttonComputeF1Salary.UseVisualStyleBackColor = true;
+            // 
+            // textBoxF1Salary
+            // 
+            this.textBoxF1Salary.Location = new System.Drawing.Point(37, 664);
+            this.textBoxF1Salary.Name = "textBoxF1Salary";
+            this.textBoxF1Salary.ReadOnly = true;
+            this.textBoxF1Salary.Size = new System.Drawing.Size(155, 20);
+            this.textBoxF1Salary.TabIndex = 33;
+            // 
+            // buttonMoveToTeam
+            // 
+            this.buttonMoveToTeam.Location = new System.Drawing.Point(459, 454);
+            this.buttonMoveToTeam.Name = "buttonMoveToTeam";
+            this.buttonMoveToTeam.Size = new System.Drawing.Size(100, 23);
+            this.buttonMoveToTeam.TabIndex = 34;
+            this.buttonMoveToTeam.Text = "csapatba átrak.";
+            this.buttonMoveToTeam.UseVisualStyleBackColor = true;
+            // 
+            // Form1Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.buttonMoveToTeam);
+            this.Controls.Add(this.textBoxF1Salary);
+            this.Controls.Add(this.buttonComputeF1Salary);
+            this.Controls.Add(this.textBoxTeamSalary);
+            this.Controls.Add(this.buttonComputeTeamSalary);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.comboBoxTeamMove);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelRecer);
+            this.Controls.Add(this.listBoxRacer);
+            this.Controls.Add(this.buttonInitializeData);
+            this.Controls.Add(this.buttonDeleteTeam);
+            this.Controls.Add(this.buttonUpdateTeam);
+            this.Controls.Add(this.buttonAddTeam);
+            this.Controls.Add(this.labelTeamName);
+            this.Controls.Add(this.textBoxTeamName);
+            this.Controls.Add(this.labelTeam);
+            this.Controls.Add(this.listBoxTeam);
+            this.Name = "Form1Form";
+            this.Text = "Forma 1.";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonInitializeData;
+        private System.Windows.Forms.Button buttonDeleteTeam;
+        private System.Windows.Forms.Button buttonUpdateTeam;
+        private System.Windows.Forms.Button buttonAddTeam;
+        private System.Windows.Forms.Label labelTeamName;
+        private System.Windows.Forms.TextBox textBoxTeamName;
+        private System.Windows.Forms.Label labelTeam;
+        private System.Windows.Forms.ListBox listBoxTeam;
+        private System.Windows.Forms.Button buttonDeleteRacer;
+        private System.Windows.Forms.Button buttonUpdateRacer;
+        private System.Windows.Forms.Button buttonAddRacer;
+        private System.Windows.Forms.Label labelRecer;
+        private System.Windows.Forms.ListBox listBoxRacer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelSalary;
+        private System.Windows.Forms.Label labelRacerAge;
+        private System.Windows.Forms.Label labelRacerName;
+        private System.Windows.Forms.ComboBox comboBoxTeamMove;
+        private System.Windows.Forms.TextBox textBoxRacerSalary;
+        private System.Windows.Forms.TextBox textBoxRacerAge;
+        private System.Windows.Forms.TextBox textBoxRacerName;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonComputeTeamSalary;
+        private System.Windows.Forms.TextBox textBoxTeamSalary;
+        private System.Windows.Forms.Button buttonComputeF1Salary;
+        private System.Windows.Forms.TextBox textBoxF1Salary;
+        private System.Windows.Forms.Button buttonMoveToTeam;
+    }
+}
+
---
+++ Forma1/Model/Racer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Model
+{
+    /// <summary>
+    /// A versenyző osztály
+    /// </summary>
+    class Racer
+    {
+        private string name;
+        private int age;
+        private int salery;
+        
+        /// <summary>
+        /// Üres konstruktor lehetetlen adatokkal.
+        /// </summary>
+        public Racer()
+        {
+            name = string.Empty;
+            age = -1;
+            salery = -1;
+        }
+
+        /// <summary>
+        /// Teljes konstruktor
+        /// </summary>
+        /// <param name="name">Versenyző neve</param>
+        /// <param name="age">Versenyző életkora</param>
+        /// <param name="salery">Versenyző fizetése</param>
+        public Racer(string name, int age, int salery)
+        {
+            this.name = name;
+            this.age = age;
+            this.salery = salery;
+        }
+
+        /// <summary>
+        /// Versenyző nevének megadása
+        /// </summary>
+        /// <param name="name">A versenyző neve</param>
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Versenyző korának megadása
+        /// </summary>
+        /// <param name="age">A versenyző kora</param>
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        /// <summary>
+        /// Versenyző fizetésének megadása
+        /// </summary>
+        /// <param name="salery">A versenyző fizetése</param>
+        public void setSalery(int salery)
+        {
+            this.salery = salery;
+        }
+
+        /// <summary>
+        /// Versenyző nevét adja vissza
+        /// </summary>
+        /// <returns>A versenyző neve</returns>
+        public string getName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// Versenyző életkorát adja vissza
+        /// </summary>
+        /// <returns>A versenyző életkorát</returns>
+        public int getAge()
+        {
+            return age;
+        }
+
+        /// <summary>
+        /// Versenyző fizetését adja vissza
+        /// </summary>
+        /// <returns>A versenyző fizetését</returns>
+        public int getSalery()
+        {
+            return salery;
+        }
+    }
+}
